8e0d45e9d6a2c2a2e85ad8688b7fffe49e95cd27
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-11-27 18:12:06 +0100

    fixed a link

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ angular.module('app', ['ngRoute'])
 .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
     .when('/home', { templateUrl: 'views/home.html' })
-    .when('/europe-2086', { templateUrl: 'views/europe.html' })
+    .when('/europe', { templateUrl: 'views/europe.html' })
     .otherwise('/home');
     
     //$locationProvider.html5Mode({enabled: true, requireBase: false});
