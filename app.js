1b9876097d84ccb8631ea1dccb3786e38ef2b78a
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-11-28 11:34:32 +0100

    Better Games p

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*global angular*/
 angular.module('app', ['ngRoute'])
 
-.controller('MainController', function($scope, $route, $location) {
+.controller('NavController', function($scope, $route, $location) {
    $scope.isActive = function(href) {
        console.log('location: ', $location.url());
        if (href == $location.url()) return 'active';
@@ -14,8 +14,25 @@ angular.module('app', ['ngRoute'])
     $routeProvider
     .when('/home', { templateUrl: 'views/home.html' })
     .when('/europe', { templateUrl: 'views/europe.html' })
-    .when('/games', { templateUrl: 'views/games.html' })
+    .when('/games', { templateUrl: 'views/games.html', controller: 'GamesController' })
     .otherwise('/home');
     
     //$locationProvider.html5Mode({enabled: true, requireBase: false});
-}]);
+}])
+
+.controller('GamesController', function($scope) {
+    $scope.games = [
+      { 
+          name: 'Dungeon Dummy',
+          about: 'A game for a game jam in Malmö. Go from room to room killing enemies to gain points.',
+          githubUrl: 'https://github.com/GlobalGameJam2015-Snittarna/QaosGameJam' },
+      { 
+          name: 'Minaret Builder', 
+          about: 'A game for Arabic Game Jam in Malmö. Stack pieces to build the highest minaret.',
+          githubUrl: 'https://github.com/Johannes-Larsson/Minaret-Builder'},
+      { 
+          name: 'Flappy Gus',
+          about: 'Flappy Bird clone, where the bird is Gus Sorola from Rooster Teeth.',
+          githubUrl: 'https://github.com/Johannes-Larsson/Flappy-Gus' }
+    ];
+})
